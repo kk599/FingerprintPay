9f726c2d11f629e9f3437005282b0c1214bae44c
kk599 <67355732+kk599@users.noreply.github.com>
2024-10-03 10:36:00 +0800

    1

--- module/reset.sh
+++ module/reset.sh
@@ -4,3 +4,10 @@ cd ${0%/*}
 cd ../3rdparty/Riru-ModuleTemplate
 git reset --hard HEAD
 git clean -df .
+
+
+cd ${0%/*}
+cd ../3rdparty/FingerprintIdentify
+
+sed -i 's/com.novoda:bintray-release:0.9/com.novoda:bintray-release:0.9.2/g' build.gradle
+
