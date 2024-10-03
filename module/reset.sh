b9964c69166c4c029c1a328de7cf51bae87511d8
kk599 <67355732+kk599@users.noreply.github.com>
2024-10-03 10:51:50 +0800

    12

--- module/reset.sh
+++ module/reset.sh
@@ -9,5 +9,8 @@ git clean -df .
 cd ../FingerprintIdentify
 
 
+sed -i "/jcenter()/a \        maven {\n            url 'https://maven.scijava.org/content/repositories/public/'\n        }" build.gradle
+
+
 sed -i 's/com.novoda:bintray-release:0.9/com.novoda:bintray-release:0.9.2/g' build.gradle
 
